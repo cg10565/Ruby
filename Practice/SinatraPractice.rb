040eb85e2bbda31383f2b744a0fdfa49c257138a
cg10232 <cg10232@outlook.com>
2025-03-02 22:51:25 -0500

    Giving proper file names + fixes to my already shoddy code.

---
+++ Practice/SinatraPractice.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require 'puma'
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  "Hello #{params['name']}!"
+end
